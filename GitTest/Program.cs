ed9559b9cfbc52fb0cd5bd4b3b0d425c1fb02837
lhx10 <1054176946@qq.com>
2016-08-06 18:32:23 +0800

    输出hello world

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,7 +10,9 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("wocao");
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+
         }
     }
 }
